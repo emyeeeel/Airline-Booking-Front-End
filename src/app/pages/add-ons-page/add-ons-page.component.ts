bc9c2779282b4044a93022e12e85954939260213
Emyeeeel <maryamielcatado@gmail.com>
2025-04-22 05:01:00 +0800

    finalized add-ons page

--- src/app/pages/add-ons-page/add-ons-page.component.ts
+++ src/app/pages/add-ons-page/add-ons-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../component/header/header.component";
 import { BookingProgressComponent } from "../../component/booking-progress/booking-progress.component";
+import { FooterComponent } from "../../component/footer/footer.component";
 
 @Component({
   selector: 'app-add-ons-page',
-  imports: [HeaderComponent, BookingProgressComponent],
+  imports: [HeaderComponent, BookingProgressComponent, FooterComponent],
   templateUrl: './add-ons-page.component.html',
   styleUrl: './add-ons-page.component.scss'
 })
